54b33acddaee8d194361568969cc20cfcec36d27
Electry <electrydev@gmail.com>
2016-08-09 19:06:28 +0200

    [SQUASH] msm: Add board files for K920
    Signed-off-by: Electry <electrydev@gmail.com>
    
    msm: k920: Add support for hall switch sensor
    
    Signed-off-by: Electry <electrydev@gmail.com>
    
    msm: k920: Fix NFC SPI gpios when KS8851 is not enabled
    
    Change-Id: I1855d78911348cd78e89bf96c4c22cf4a332d109
    Signed-off-by: Michal Chvíla <electrydev@gmail.com>
    
    msm: k920: Cleanup board files
    
    Signed-off-by: Michael Sky <electrydev@gmail.com>

---
+++ arch/arm/mach-msm/k920/lenovo_nv.h
@@ -0,0 +1,11 @@
+#ifndef _LENOVO_NV_H_
+#define _LENOVO_NV_H_
+/*
+ * Based on the work of Tony Sun
+ * Zhenglq: Add for Get nv data from modem using SMEM.
+*/
+
+void __init lenovo_nv_init(void);
+
+#endif
+
